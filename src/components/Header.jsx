c812b06156b4bbb97197a51ea18f15e99517426a
임효진 <imhyojin@imhyojicBookPro.Davolink>
2022-10-14 15:18:09 +0900

    Completed to add home UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,44 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <STHeader>
+      <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 20 20'
+        height='24'
+        width='24'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path d='M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z'></path>
+      </svg>
+      <STHeaderTitle>3조의 투두리스트</STHeaderTitle>
+    </STHeader>
+  );
 };
+
+const STHeader = styled.header`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  flex-direction: row;
+  height: 45px;
+  background-color: rgb(255, 255, 255);
+  border-bottom: 1px solid rgb(221, 221, 221);
+  padding: 0px 12px;
+`;
+const STHeaderTitle = styled.div`
+  font-size: 24px;
+  box-sizing: border-box;
+  padding: 0px;
+  margin: 0px;
+  text-decoration: none;
+  outline: none;
+  font-family: "Noto Sans KR", sans-serif;
+`;
 
 export default Header;
